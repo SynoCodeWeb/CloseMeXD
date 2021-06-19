64ee64efb3fe66623d46b6a446bb32b40955ad10
SynoCodeWeb <romanet70@yahoo.com>
2021-06-19 14:56:28 +0200

    First commit

---
+++ CloseMeXD/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace CloseMeXD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFunct1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnFunct1
+            // 
+            this.btnFunct1.Location = new System.Drawing.Point(37, 28);
+            this.btnFunct1.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFunct1.Name = "btnFunct1";
+            this.btnFunct1.Size = new System.Drawing.Size(117, 109);
+            this.btnFunct1.TabIndex = 1;
+            this.btnFunct1.Text = "CLOSE ME";
+            this.btnFunct1.UseVisualStyleBackColor = true;
+            this.btnFunct1.Click += new System.EventHandler(this.btnFunct1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 169);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(197, 32);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(212, 206);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnFunct1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFunct1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ CloseMeXD/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CloseMeXD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            GlobalMouseHandler gmh = new GlobalMouseHandler();
+            gmh.TheMouseMoved += new MouseMovedEvent(gmh_TheMouseMoved);
+            Application.AddMessageFilter(gmh);
+            this.FormBorderStyle = FormBorderStyle.None;
+            InitializeComponent();
+        }
+
+        private void btnFunct1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("YU AR IDIOT AH AH AH AHAHAHAAAHAHAHAAA!");
+            Form1 frm = new Form1();
+            frm.Show();
+        }
+
+        void gmh_TheMouseMoved()
+        {
+            Point cur_pos = System.Windows.Forms.Cursor.Position;
+            progressBar1.Increment(1);
+            //1366 - X
+            //768 - Y
+            int countX = cur_pos.X - this.Location.X;
+            if(countX <=100)
+                this.Location = new Point(this.Location.X+100, this.Location.Y);
+            else
+                this.Location = new Point(this.Location.X - 100, this.Location.Y);
+
+            int countY = cur_pos.Y - this.Location.Y;
+            if (countY <= 100)
+                this.Location = new Point(this.Location.X, this.Location.Y + 100);
+            else
+                this.Location = new Point(this.Location.X , this.Location.Y - 100);
+            if (this.Location.X <= 0 || this.Location.X >= 1366)
+            {
+                if (this.Location.X >= 1366)
+                    this.Location = new Point(this.Location.X - 500, this.Location.Y + 100);
+                if (this.Location.X <= 0)
+                    this.Location = new Point(this.Location.X + 500, this.Location.Y + 100);
+            }
+            if (this.Location.Y <= 0 || this.Location.Y >= 768)
+            {
+                if(this.Location.Y >= 768)
+                    this.Location = new Point(this.Location.X, this.Location.Y - 400);
+                if (this.Location.Y <= 0)
+                    this.Location = new Point(this.Location.X, this.Location.Y + 400);
+            }
+
+            //MessageBox.Show(this.Location.ToString());
+        }
+    }
+    public delegate void MouseMovedEvent();
+
+    public class GlobalMouseHandler : IMessageFilter
+    {
+        private const int WM_MOUSEMOVE = 0x0200;
+
+        public event MouseMovedEvent TheMouseMoved;
+
+        #region IMessageFilter Members
+
+        public bool PreFilterMessage(ref Message m)
+        {
+            if (m.Msg == WM_MOUSEMOVE)
+            {
+                if (TheMouseMoved != null)
+                {
+                    TheMouseMoved();
+                }
+            }
+            // Always allow message to continue to the next filter control
+            return false;
+        }
+
+        #endregion
+    }
+}
